3a6a1125d7cb6670b51b310258d4e51ba555cfea
kijimaD <norimaking777@gmail.com>
2023-01-10 23:19:10 +0900

    feat: db connect

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,7 +26,7 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => 'localhost',
+        'hostname' => 'db',
         'username' => '',
         'password' => '',
         'database' => '',
